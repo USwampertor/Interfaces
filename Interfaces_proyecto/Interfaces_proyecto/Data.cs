312c4af0058793b11c028f92cc18ba5825a77816
Alberto Villa <idv16c.jvilla@uartesdigitales.edu.mx>
2018-02-21 22:31:55 -0600

    +Cree clases Data y TeamData, interfaz DataCapsule *Data implementa DataCapsule

---
+++ Interfaces_proyecto/Interfaces_proyecto/Data.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaces_proyecto
+{
+    class Data : DataCapsule
+    {
+        List<TeamData> teamList = new List<TeamData>();
+        List<string> names = new List<string>();
+
+        public void DataFetch(ListBox nameList)
+        {
+            for (int i = 0; i < nameList.Items.Count; i++)
+            {
+                names.Add(nameList.Items[i].ToString());
+            }
+        }
+
+        public void SortNames(int members)
+        {
+            Random rand = new Random();
+            List<string> namePool = names;
+            while (namePool.Count > 0)
+            {
+                TeamData newTeam = new TeamData();
+                for (int i = 0; i < members && namePool.Count > 0; i++)
+                {
+                    int j = rand.Next(0, namePool.Count - 1);
+                    newTeam.Team.Add(namePool[j]);
+                    namePool.RemoveAt(j);
+                }
+            }
+        }
+
+        public List<TeamData> GetTeams()
+        {
+            return teamList;
+        }
+    }
+}
