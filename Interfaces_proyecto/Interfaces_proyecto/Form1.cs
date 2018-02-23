479d609a78c31007ad20e6212c3b18c21ca4ff84
Rossana25 <idv16a.rloyola@uartesdigitales.edu.mx>
2018-02-22 22:18:07 -0600

    Cambios de interface

--- Interfaces_proyecto/Interfaces_proyecto/Form1.cs
+++ Interfaces_proyecto/Interfaces_proyecto/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Interfaces_proyecto
 {
@@ -15,6 +16,57 @@ namespace Interfaces_proyecto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Beep.Play();
+
+            Color BackColor = Color.Green;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Exclamation.Play();
+            
+            //SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            //simpleSound.Play();
+
+            Color BackColor = Color.Red;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Asterisk.Play();
+
+            //SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            //simpleSound.Play();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Hand.Play();
+
+            //SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            //simpleSound.Play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Question.Play();
+
+            //SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            //simpleSound.Play();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
